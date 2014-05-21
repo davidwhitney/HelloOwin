20b51c1b46f5710aac92842dc94ea9d161e2abc6
David Whitney <david@davidwhitney.co.uk>
2014-05-21 12:51:17 +0100

    Added example

---
+++ HelloOwin.Middleware/MiddlewareDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+namespace HelloOwin.Middleware
+{
+    public class MiddlewareDemo
+    {
+        private readonly AppFunc _next;
+
+        public MiddlewareDemo(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            using (var writer = new StreamWriter((Stream)env["owin.ResponseBody"]))
+            {
+                writer.Write("Modification to the stream...");
+                writer.Flush();
+            }
+            return _next(env);
+        }
+    }
+}
---
+++ HelloOwin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HelloOwin.Middleware;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace HelloOwin
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            using (WebApp.Start<Startup>("http://localhost:8080/"))
+            {
+                Console.WriteLine("Started Katana host");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<MiddlewareDemo>();
+        }
+    }
+}
